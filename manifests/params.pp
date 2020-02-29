2d4e7b786e895422380dd1c6c5238846b803af2b
root <root@ipmasqman1c.mylabserver.com>
2020-02-29 16:26:27 +0000

    Add install class with Centos 7/ Debian support

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# Summary
+#   Operating system-related variables and others.
+
+class apache::params {
+  $install_ensure = 'present'
+
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
