d4aa4508ccd2933cb9b08945fbf8f6bf9c8a5cfc
root <root@ipmasqman1c.mylabserver.com>
2020-03-01 14:36:08 +0000

    Refactored module to use Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,11 +6,20 @@
 ## }
 ## }
 
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+
+#params style
+#class apache::install (
+#  $install_name   = $apache::params::install_name,
+# $install_ensure = $apache::params::install_ensure,
+#) inherits apache::params {
+#  package { "${install_name}":
+#    ensure => $install_ensure,
+#  }
+#}
+
+#hiera style
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,20 @@
 # Summary
 #   Operating system-related variables and others.
 
-class apache::params {
-  $install_ensure = 'present'
 
-  case $::osfamily {
-    'RedHat': {
-      $install_name = 'httpd'
-    }
-    'Debian': {
-      $install_name = 'apache2'
-    }
-  }
-}
+#commented out for Hiera
+
+
+#params.pp example overridden with Hiera
+#class apache::params {
+#  $install_ensure = 'present'
+
+#  case $::osfamily {
+#    'RedHat': {
+#      $install_name = 'httpd'
+#    }
+#    'Debian': {
+#      $install_name = 'apache2'
+#    }
+#  }
+#}
